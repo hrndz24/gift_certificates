ac2a5e8599e449647e385fd8d2e4d09982daa42f
Natalie <natalli.buyanova.01@mail.ru>
2020-11-04 13:45:51 +0300

    Move datasourse properties to another file

--- repository/src/main/java/com/epam/esm/config/SpringJdbcConfig.java
+++ repository/src/main/java/com/epam/esm/config/SpringJdbcConfig.java
@@ -1,8 +1,11 @@
 package com.epam.esm.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
@@ -10,19 +13,27 @@ import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan("com.epam.esm")
+@PropertySource("classpath:db.properties")
 public class SpringJdbcConfig {
+
+    private Environment environment;
+
+    @Autowired
+    public SpringJdbcConfig(Environment environment) {
+        this.environment = environment;
+    }
 
     @Bean
     public DataSource mysqlDataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/gift_certificate?useUnicode=true&serverTimezone=UTC");
-        dataSource.setUsername("root");
-        dataSource.setPassword("si3Llega7la5");
+        dataSource.setUrl(environment.getProperty("spring.datasource.url.1"));
+        dataSource.setDriverClassName(environment.getProperty("spring.datasource.driver-class-name.1"));
+        dataSource.setUsername(environment.getProperty("spring.datasource.username.1"));
+        dataSource.setPassword(environment.getProperty("spring.datasource.password.1"));
         return dataSource;
     }
 
-    @Bean(name = "JdbcTemplate")
+    @Bean
     public JdbcTemplate getJdbcTemplate() {
         return new JdbcTemplate(mysqlDataSource());
     }
